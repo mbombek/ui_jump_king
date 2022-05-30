5d2c6bf26ccee87d4910fe7c693da71756a1aff1
JakobSkornik <skornikjakobs@gmail.com>
2022-05-30 18:19:00 +0200

    Logs

--- sketch.js
+++ sketch.js
@@ -172,10 +172,10 @@ function draw() {
     textSize(32);
     fill(255, 255, 255);
     text("FPS: " + previousFrameRate, width - 160, 35);
-    text("Actions: " + player.brain.numberOfActions, 30, 35);
-    text("Best Height: " + player.bestHeightReached, 240, 35);
-    text("Current: " + ~~player.GetGlobalHeight(), 560, 35);
-    text("Level: " + player.currentLevelNo, 800, 35);
+    text("Actions: " + player.brain.numberOfActions, 10, 35);
+    text("Best Height: " + player.bestHeightReached, 300, 35);
+    text("Current: " + ~~player.GetGlobalHeight(), 600, 35);
+    text("Level: " + player.currentLevelNo, 850, 35);
   }
 }
 
